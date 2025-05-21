6be63451dd1bdfa831975b630329628939d75e55
aabccd021 <aabccd021@gmail.com>
2025-05-21 21:03:50 +0700

    Revert "chore: Format flake.nix code style"
    This reverts commit 9dbe2937cdf7f205c29fd4b618ba313cbc885057.

--- flake.nix
+++ flake.nix
@@ -41,7 +41,6 @@
       treefmtEval = inputs.treefmt-nix.lib.evalModule pkgs {
         projectRootFile = "flake.nix";
         programs.nixfmt.enable = true;
-        programs.nixfmt.strict = true;
         programs.prettier.enable = true;
         programs.shfmt.enable = true;
         programs.shellcheck.enable = true;
@@ -87,7 +86,9 @@
         '';
       };
 
-      devShells.default = pkgs.mkShellNoCC { buildInputs = [ pkgs.nixd ]; };
+      devShells.default = pkgs.mkShellNoCC {
+        buildInputs = [ pkgs.nixd ];
+      };
 
       packages = devShells // {
         updateDependencies = updateDependencies;
